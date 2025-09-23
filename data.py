22128d36c316b89b666eb8e7dc1489d320d3dcc3
Gikaeh <ginonicosia@yahoo.com>
2025-09-23 01:41:17 -0700

    data will redo

--- data.py
+++ data.py
@@ -1,10 +1,13 @@
-from pybaseball import schedule_and_record
+from pybaseball import schedule_and_record, batting_stats_bref
 import pandas as pd
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 teamsAbbr = [
-    'BOS','BAL','NYY','TB','TOR','CHW','CLE','DET','KC','MIN',
+    'BOS','BAL','NYY','TBR','TOR','CHW','CLE','DET','KCR','MIN',
     'HOU','LAA','OAK','SEA','TEX','ARI','ATL','MIA','NYM','PHI',
-    'CHC','CIN','COL','LAD','MIL','STL','PIT','SD','SF','WSH'
+    'CHC','CIN','COL','LAD','MIL','STL','PIT','SD','SF','WSN'
 ]
 
 errorWatch = []
@@ -12,27 +15,51 @@ errorWatch = []
 
 columnsToKeep = ['Date', 'Tm', 'Home_Away', 'Opp', 'W/L', 'R', 'RA', 'D/N', 'Streak']
 
+# for year in range(2020, 2025):
+#     gameData = []
+
+#     for team in teamsAbbr:
+#         try:
+#             team_data = schedule_and_record(year, team)
+#             filteredData = team_data[columnsToKeep]
+#             gameData.append(filteredData)
+#         except Exception as e:
+#             errorWatch.append(f"Skipped {team} for {year} due to an error: {e}")        
+
+#     masterDf = pd.concat(gameData, ignore_index=True)
+#     masterDf['unique_game_id'] = masterDf.apply(
+#         lambda row: f"{row['Date']}_{sorted([row['Tm'], row['Opp']])[0]}_{sorted([row['Tm'], row['Opp']])[1]}", 
+#         axis=1
+#     )
+
+#     masterDf.drop_duplicates(subset='unique_game_id', keep='first', inplace=True)
+#     masterDf.drop(columns='unique_game_id', inplace=True)
+
+#     masterDf.to_csv(f'./training_data/{year}_MLB_Season.csv', index=False)
+
+# print(errorWatch)
+
 for year in range(2020, 2025):
     gameData = []
 
     for team in teamsAbbr:
         try:
-            team_data = schedule_and_record(year, team)
-            filteredData = team_data[columnsToKeep]
-            gameData.append(filteredData)
+            team_data = batting_stats_bref(year)
+            print(team_data)
+            # filteredData = team_data[columnsToKeep]
+            # gameData.append(filteredData)
         except Exception as e:
-            # Use a more specific exception to not hide other errors
             errorWatch.append(f"Skipped {team} for {year} due to an error: {e}")        
 
-    masterDf = pd.concat(gameData, ignore_index=True)
-    masterDf['unique_game_id'] = masterDf.apply(
-        lambda row: f"{row['Date']}_{sorted([row['Tm'], row['Opp']])[0]}_{sorted([row['Tm'], row['Opp']])[1]}", 
-        axis=1
-    )
+    # masterDf = pd.concat(gameData, ignore_index=True)
+    # masterDf['unique_game_id'] = masterDf.apply(
+    #     lambda row: f"{row['Date']}_{sorted([row['Tm'], row['Opp']])[0]}_{sorted([row['Tm'], row['Opp']])[1]}", 
+    #     axis=1
+    # )
 
-    masterDf.drop_duplicates(subset='unique_game_id', keep='first', inplace=True)
-    masterDf.drop(columns='unique_game_id', inplace=True)
+    # masterDf.drop_duplicates(subset='unique_game_id', keep='first', inplace=True)
+    # masterDf.drop(columns='unique_game_id', inplace=True)
 
-    masterDf.to_csv(f'{year}_MLB_Season.csv', index=False)
+    # masterDf.to_csv(f'./training_data/{year}_MLB_Season.csv', index=False)
 
-print(errorWatch)
+# print(errorWatch)

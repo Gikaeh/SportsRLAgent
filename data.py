d61a00a07323bc98488f594db0d115a4174e7f92
Gikaeh <ginonicosia@yahoo.com>
2025-09-23 19:02:24 -0700

    Implementation of park_factor_scraping

--- data.py
+++ data.py
@@ -1,65 +1,116 @@
 from pybaseball import schedule_and_record, batting_stats_bref
 import pandas as pd
 import warnings
+from park_factor_scraping import scrape_park_factors
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-teamsAbbr = [
-    'BOS','BAL','NYY','TBR','TOR','CHW','CLE','DET','KCR','MIN',
-    'HOU','LAA','OAK','SEA','TEX','ARI','ATL','MIA','NYM','PHI',
-    'CHC','CIN','COL','LAD','MIL','STL','PIT','SD','SF','WSN'
-]
+#Grabs game data
+def grabGameData(year: int):
+    teams_abbr = [
+        'BOS','BAL','NYY','TBR','TOR','CHW','CLE','DET','KCR','MIN',
+        'HOU','LAA','OAK','SEA','TEX','ARI','ATL','MIA','NYM','PHI',
+        'CHC','CIN','COL','LAD','MIL','STL','PIT','SDP','SFG','WSN'
+    ]
+    error_watch = []
+    columns_to_keep = ['Date', 'Tm', 'Home_Away', 'Opp', 'W/L', 'R', 'RA', 'D/N']
+    game_data = []
 
-errorWatch = []
-
-
-columnsToKeep = ['Date', 'Tm', 'Home_Away', 'Opp', 'W/L', 'R', 'RA', 'D/N', 'Streak']
-
-# for year in range(2020, 2025):
-#     gameData = []
-
-#     for team in teamsAbbr:
-#         try:
-#             team_data = schedule_and_record(year, team)
-#             filteredData = team_data[columnsToKeep]
-#             gameData.append(filteredData)
-#         except Exception as e:
-#             errorWatch.append(f"Skipped {team} for {year} due to an error: {e}")        
-
-#     masterDf = pd.concat(gameData, ignore_index=True)
-#     masterDf['unique_game_id'] = masterDf.apply(
-#         lambda row: f"{row['Date']}_{sorted([row['Tm'], row['Opp']])[0]}_{sorted([row['Tm'], row['Opp']])[1]}", 
-#         axis=1
-#     )
-
-#     masterDf.drop_duplicates(subset='unique_game_id', keep='first', inplace=True)
-#     masterDf.drop(columns='unique_game_id', inplace=True)
-
-#     masterDf.to_csv(f'./training_data/{year}_MLB_Season.csv', index=False)
-
-# print(errorWatch)
-
-for year in range(2020, 2025):
-    gameData = []
-
-    for team in teamsAbbr:
+    for team in teams_abbr:
         try:
-            team_data = batting_stats_bref(year)
-            print(team_data)
-            # filteredData = team_data[columnsToKeep]
-            # gameData.append(filteredData)
+            team_data = schedule_and_record(year, team)
+            filtered_data = team_data[columns_to_keep]
+            cleaned_data = cleanupData(filtered_data)
+            fixed_dates_data = fixDatesInData(cleaned_data, year)
+            game_data.append(fixed_dates_data)
         except Exception as e:
-            errorWatch.append(f"Skipped {team} for {year} due to an error: {e}")        
+            error_watch.append(f"Skipped {team} for {year} due to an error: {e}")        
 
-    # masterDf = pd.concat(gameData, ignore_index=True)
-    # masterDf['unique_game_id'] = masterDf.apply(
-    #     lambda row: f"{row['Date']}_{sorted([row['Tm'], row['Opp']])[0]}_{sorted([row['Tm'], row['Opp']])[1]}", 
-    #     axis=1
-    # )
+    print(error_watch)
+    return game_data
 
-    # masterDf.drop_duplicates(subset='unique_game_id', keep='first', inplace=True)
-    # masterDf.drop(columns='unique_game_id', inplace=True)
+#Cleans up data renaming columns and removing 'Home_Away' after use
+def cleanupData(loaded_data: pd.DataFrame):
+    updated_data = loaded_data[loaded_data['Home_Away'] != '@']
+    updated_data.drop(columns='Home_Away', inplace=True)
 
-    # masterDf.to_csv(f'./training_data/{year}_MLB_Season.csv', index=False)
+    updated_data.rename(columns={'Date': 'date', 'Tm': 'home_team', 'Opp': 'away_team', 'W/L': 'win', 'R': 'home_runs_scored', 'RA': 'away_runs_scored', 'D/N': 'day'}, inplace=True)
 
-# print(errorWatch)
+    updated_data['win'].replace({'W': 1, 'W-wo': 1, 'L': 0, 'L-wo': 0}, inplace=True)
+    updated_data['day'].replace({'D': 1, 'N': 0}, inplace=True)
+
+    return updated_data
+
+#Fixes the dates to have year and be - separated (called in grabGameData)
+def fixDatesInData(loaded_data: pd.DataFrame, year: int):
+    months_name_to_num = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }
+    updated_data = loaded_data
+
+    for i in range(len(updated_data)):
+        month = months_name_to_num[updated_data.iloc[i,0].split(' ')[1]]
+        day = updated_data.iloc[i,0].split(' ')[2]
+
+        if month < 10:
+            month = f'0{month}'
+        if int(day) < 10:
+            day = f'0{day}'
+
+        updated_data.iloc[i,0] = f'{year}-{month}-{day}'
+    
+    return updated_data
+
+def grabParkFactor(loaded_data: pd.DataFrame, year: int):
+    park_factor_data = scrape_park_factors(year)
+    
+    for i in range(len(loaded_data)):
+        team = loaded_data.iloc[i, 1]
+        
+        if len(park_factor_data[park_factor_data['team'] == team]) != 0:
+            loaded_data.iloc[i, len(loaded_data)]
+        else:
+            print(False)
+
+
+#Organize columns
+def organizeColumns(loaded_data: pd.DataFrame):
+    column_order = [
+        'date', 'home_team', 'away_team', 'game_id', 'opening_bet_total', 
+        'closing_bet_total', 'actual_total_runs', 'over_hit', 'total_diff_vs_line', 'home_runs_per_game_L10', 
+        'away_runs_per_game_L10', 'home_team_ops_L10', 'away_team_ops_L10', 'home_hr_per_game_L10',	'away_hr_per_game_L10', 
+        'home_team_era_L10', 'away_team_era_L10', 'home_team_whip_L10',	'away_team_whip_L10', 'home_sp_era',
+        'away_sp_era', 'home_sp_whip', 'away_sp_whip', 'home_sp_k_per_9', 'away_sp_k_per_9',
+        'day', 'park_factor', 'line_movement', 'combined_offense_L10', 'combined_era_L10', 'sp_era_average'
+    ]
+
+    updated_data = loaded_data[column_order]
+
+    return updated_data
+
+def createCsv(load_data: pd.DataFrame, year):
+    master_df = pd.concat(load_data, ignore_index=True)
+    master_df['game_id'] = master_df.apply(
+        lambda row: f"{row['date']}_{sorted([row['home_team'], row['away_team']])[0]}_{sorted([row['home_team'], row['away_team']])[1]}", 
+        axis=1
+    )
+
+    master_df.to_csv(f'./training_data/{year}_MLB_Season.csv', index=False)
+
+# for i in range(2020, 2025):
+#     data = grabGameData(i)
+#     data = grabParkFactor(data, i)
+
+data = pd.read_csv('./training_data/2020_MLB_Season.csv')
+print(grabParkFactor(data, 2020))
---
+++ park_factor_scraping.py
@@ -0,0 +1,68 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import json
+import re
+
+def scrape_park_factors(year: int) -> pd.DataFrame:
+    team_mapping = {
+        'Athletics': 'OAK', 'White Sox': 'CHW', 'Cardinals': 'STL', 'Astros': 'HOU',
+        'Padres': 'SDP', 'Guardians': 'CLE', 'Blue Jays': 'TOR', 'Brewers': 'MIL',
+        'Reds': 'CIN', 'Nationals': 'WSN', 'Mariners': 'SEA', 'Royals': 'KC',
+        'Tigers': 'DET', 'Red Sox': 'BOS', 'Orioles': 'BAL', 'Angels': 'LAA',
+        'Pirates': 'PIT', 'D-backs': 'ARI', 'Yankees': 'NYY', 'Dodgers': 'LAD',
+        'Phillies': 'PHI', 'Rockies': 'COL', 'Rangers': 'TEX', 'Mets': 'NYM',
+        'Marlins': 'MIA', 'Braves': 'ATL', 'Giants': 'SFG', 'Twins': 'MIN',
+        'Cubs': 'CHC', 'Rays': 'TBR'
+    }
+    url = f'https://baseballsavant.mlb.com/leaderboard/statcast-park-factors?type=year&year={year}&batSide=&stat=index_wOBA&condition=All&rolling=1&parks=mlb'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    
+    try:
+        response = requests.get(url, headers=headers, timeout=30)
+        response.raise_for_status()
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        script_tags = soup.find_all('script')
+        
+        for script in script_tags:
+            if script.string:
+                data_match = re.search(r'var data = (\[.*?\]);', script.string, re.DOTALL)
+                if data_match:
+                    try:
+                        data = json.loads(data_match.group(1))                        
+                        park_factors = []
+                        
+                        for park in data:
+                            team_name = park.get('name_display_club', '')
+                            team_abbr = team_mapping.get(team_name, team_name[:3].upper())
+                            
+                            park_factor = {
+                                'year': year,
+                                'team': team_abbr,
+                                # 'venue': park.get('venue_name', ''),
+                                'park_factor': int(park.get('index_woba', 100)) / 100,  # Normalize to 1.0 = average
+                                # 'runs_factor': park.get('index_runs', 100) / 100,
+                                # 'hr_factor': park.get('index_hr', 100) / 100,
+                                # 'woba_factor': int(park.get('index_woba', 100)) / 100,
+                                # 'bb_factor': park.get('index_bb', 100) / 100,
+                                # 'so_factor': park.get('index_so', 100) / 100
+                            }
+                            park_factors.append(park_factor)
+                        
+                        df = pd.DataFrame(park_factors)
+                        return df
+                        
+                    except json.JSONDecodeError as e:
+                        print(f"Error parsing JSON: {e}")
+                        continue
+        
+        print(f"No park factors data found for {year}")
+        return pd.DataFrame()
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for {year}: {e}")
+        return pd.DataFrame()
